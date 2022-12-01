9349ba7689b4ae6f766813831ddd4a8c03a82603
brendanball4 <ball.brendan10@gmail.com>
2022-12-01 00:06:21 -0700

    added in image for readme

--- src/data/portfolio.js
+++ src/data/portfolio.js
@@ -2,19 +2,31 @@ export default [
     {
         title: 'HR Web App',
         imgUrl: 'assets/images/foodbank.PNG',
-        stack: [ 'Collaboration Project', 'HTML', 'CSS', 'Bootstrap 4', 'C#', 'Blazor', 'Entity Framework', 'MariaDB', 'Docker'],
+        stack: [ 'Collaboration', 'HTML', 'CSS', 'Bootstrap 4', 'C#', 'Blazor', 'Entity Framework', 'MariaDB', 'Docker'],
         link: 'https://github.com/cdmoss/hrmanager-blazorapp',
     },
     {
         title: 'Developer Portfolio',
         imgUrl: 'assets/images/DevPortfolio.PNG',
-        stack: [ 'Solo Project', 'React', 'Tailwind', 'Vite'],
+        stack: [ 'Solo', 'React', 'Tailwind', 'Vite'],
         link: 'https://github.com/brendanball4/PortfolioJSXTailwind',
     },
     {
         title: 'Admin Web App',
-        imgUrl: 'assets/images/admin.PNG',
-        stack: ['School Project', 'HTML', 'CSS', 'PHP8.1', 'Bootstrap 4', 'MySQL'],
+        imgUrl: 'assets/images/comp_repair.png',
+        stack: ['School', 'HTML', 'CSS', 'PHP8.1', 'Bootstrap 4', 'MySQL'],
         link: 'https://github.com/cdmoss/3660-Project',
+    },
+    {
+        title: 'Unity Game Development',
+        imgUrl: 'assets/images/darksoulstut.PNG',
+        stack: ['C#', 'Unity Game Engine'],
+        link: 'https://github.com/brendanball4/DSTut',
+    },
+    {
+        title: 'Blender',
+        imgUrl: 'assets/images/isometricroom.png',
+        stack: ['Blender'],
+        link: 'https://github.com/brendanball4/blender-projects',
     },
 ]
