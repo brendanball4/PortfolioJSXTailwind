6b2698253db68ca468cf49227a6bbd13f45f856d
brendanball4 <ball.brendan10@gmail.com>
2024-03-08 23:15:27 -0700

    Attempting to fix photo not displaying

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,33 +1,33 @@
 import React from 'react';
 import me from '/PortfolioImages/Personal/me.jpg'
-import TypeAnimation from 'react-type-animation';
+//import TypeAnimation from 'react-type-animation';
 
 function Intro() {
-    const ExampleComponent = () => {
-        return (
-          <TypeAnimation
-            sequence={[
-              'Full Stack Developer',
-              1000,
-              'Software Developer',
-              1000,
-              'Web Developer',
-              1000,
-              'Front End Developer',
-              1000,
-              'Back End Developer',
-              1000,
-              'Database Administrator',
-              1000,
-            ]}
-            wrapper="span"
-            speed={30}
-            className='text-black dark:text-orange-300'
-            style={{ fontSize: '2em', display: 'inline-block' }}
-            repeat={Infinity}
-          />
-        );
-      };
+    // const ExampleComponent = () => {
+    //     return (
+    //       <TypeAnimation
+    //         sequence={[
+    //           'Full Stack Developer',
+    //           1000,
+    //           'Software Developer',
+    //           1000,
+    //           'Web Developer',
+    //           1000,
+    //           'Front End Developer',
+    //           1000,
+    //           'Back End Developer',
+    //           1000,
+    //           'Database Administrator',
+    //           1000,
+    //         ]}
+    //         wrapper="span"
+    //         speed={30}
+    //         className='text-black dark:text-orange-300'
+    //         style={{ fontSize: '2em', display: 'inline-block' }}
+    //         repeat={Infinity}
+    //       />
+    //     );
+    //   };
 
     return (
         <div className="pt-20 pb-6 flex justify-center items-center flex-col md:flex-row">
@@ -41,7 +41,7 @@ function Intro() {
             <div className='md:ml-12 basis-2/3 text-center md:text-left'>
                 <div>
                 <h1 className='text-4xl md:text-6xl dark:text-white mb-1 md:mb-3 font-bold'>Hi, I'm <a className='text-purple-500 dark:text-gray-500'>Brendan Ball</a></h1>
-                <ExampleComponent />
+                {/*<ExampleComponent />*/}
                 <p className='text-md max-w-xl font-bold mx-auto md:mx-0'>
                 As a full stack developer, I specialize in crafting applications from the ground up, leveraging my expertise in C#, Python, C++, and various associated frameworks to bring ideas to life. 
                 I have dedicated myself to both solo and collaborative development efforts that are featured in the Portfolio section. 
