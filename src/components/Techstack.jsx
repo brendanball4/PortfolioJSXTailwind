366035f461781b8db9575251d1128215256a527f
brendanball4 <ball.brendan10@gmail.com>
2024-05-26 20:00:35 -0600

    Fix a casing error with import statement.

--- src/components/Techstack.jsx
+++ src/components/Techstack.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Title from "./Title";
-import techstack from '../data/Techstack';
+import techstack from '../data/techstack';
 import TechstackItem from './TechstackItem';
 
 function Techstack() {
