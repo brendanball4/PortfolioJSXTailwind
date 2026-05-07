b95af45de2dc1ed5ac9dc0c38f4c3bc98c00f43e
brendanball4 <ball.brendan10@gmail.com>
2026-05-06 23:00:20 -0600

    Standardize timeline dates and durations

--- src/data/timeline.js
+++ src/data/timeline.js
@@ -14,25 +14,25 @@ export default [
     {
         year: "2024",
         title: "Full-stack Developer at Medicine Hat College",
-        duration: "4 mo (Contract, May-Aug)",
+        duration: "4 mo (Contract)",
         details: "I worked with Medicine Hat College to create a solar energy monitoring system using C#, .NET Core, MudBlazor, and Selenium. The application aggregates and analyzes data from multiple solar arrays, providing real-time insights for optimizing energy usage.",
     },
     {
-        year: "2020-24",
+        year: "2020 - 2024",
         title: "Completed Bachelor's Degree in Computer Science",
-        duration: "4 years",
+        duration: "4 yr",
         details: "I earned my Bachelor's degree in Computer Science, completing a range of projects and studies focused on software development and technology.",
     },
     {
-        year: "2020-21",
+        year: "2020 - 2021",
         title: "Full-stack Developer at Medicine Hat Food Bank",
-        duration: "1 1/2 years",
+        duration: "1.5 yr",
         details: "Developed a robust HR application for the Medicine Hat Food Bank, integrating C#, Blazor, Entity Framework Core, and MariaDB to streamline operations.",
     },
     {
-        year: "2018-20",
+        year: "2018 - 2020",
         title: "Completed Software Development Diploma",
-        duration: "2 years",
+        duration: "2 yr",
         details: "Received my Software Development Diploma from Medicine Hat College, where I also became a member of Phi Theta Kappa.",
     },
 ]
